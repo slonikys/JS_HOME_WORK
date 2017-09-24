6e99b2bfaaf271feecccb14c2c4d61466b9d8cdf
Dmitrij Sotnikov <terorl2@mail.ru>
2017-09-24 17:01:14 +0300

    finish

--- hw2.github.io/js/main.js
+++ hw2.github.io/js/main.js
@@ -6,7 +6,6 @@ function showArrElements() {
   for (var i = 0; i < arr.length; i++) {
     p.innerHTML += +arr[i] + " <br>";
   }
-  console.log(arr);
 }
 
 function pop() {
@@ -20,5 +19,11 @@ function shift() {
 function push() {
   var value = document.getElementById('value').value,
     index = document.getElementById('index').value;
+  if (isNaN(index)) {
+    p.innerHTML = " Не вводите БУКВЫ в индекс только числа )";
+  } else if (index > (arr.length - 1)) {
+    arr[(arr.length - 1) + (index - (arr.length - 1))] = value;
+  }
   return arr[index] = value;
+
 }
