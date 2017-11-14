fecf230d431cb4023d5f2c09280eb742108bfeb7
Dmitrij Sotnikov <terorl2@mail.ru>
2017-11-14 12:40:54 +0200

    reworked dz

--- hw5.github.io/js/main.js
+++ hw5.github.io/js/main.js
@@ -2,21 +2,16 @@
   var field = document.getElementById('field');
   var answer = document.querySelector('#coords');
   var block = document.querySelector('#field');
-function getCoordinates(event){
-    answer.innerHTML = 'X: ' + event.clientX + ', Y: ' + event.clientY;
-}
 
-let block = document.querySelector('#field');
-block.addEventListener('mousemove', getCoordinates);
+  function getCoordinates(event) {
+      answer.innerHTML = 'X: ' + event.clientX + ', Y: ' + event.clientY;
+  }
 
-
-
-
-
-
-  // field.onmousemove = function(e) {
-  //     document.getElementById('coords').innerHTML = "по x : " + e.clientX + ' , по Y: ' + e.clientY;
-  // };
+  function mouseOutOfField() {
+      answer.innerHTML = 'Переведите МЫШЬ на блок Для ПОЛУЧЕНИЯ координат';
+  }
+  block.addEventListener('mousemove', getCoordinates);
+  block.addEventListener('mouseout', mouseOutOfField);
   //task2
   var openF = document.querySelector('#task2');
   openF.addEventListener('dblclick', openThisFolder);
@@ -25,10 +20,18 @@ block.addEventListener('mousemove', getCoordinates);
       openF.classList.toggle('open');
   }
   // task3
-  var cont = document.querySelector('#task3'),
-      button3 = document.querySelector('#button3');
+  var cont = document.querySelector('#task3');
+  var button3 = document.querySelector('#button3');
 
   function createblock() {
+
+      function doCircle() {
+          this.classList.add('circle');
+      }
+
+      function backtoblock() {
+          this.classList.remove('circle');
+      }
       for (var i = 0; i < 300; i++) {
           var element = document.createElement('div');
           element.classList.add('block');
@@ -38,12 +41,12 @@ block.addEventListener('mousemove', getCoordinates);
       block.forEach(function(elem) {
           elem.addEventListener('mouseover', doCircle);
           elem.addEventListener('mouseout', backtoblock);
-      })
+      });
   }
   button3.addEventListener('click', createblock);
   // Task 4
-  var parentDiv = document.querySelector('#task4'),
-      conn = document.querySelector('#container');
+  var parentDiv = document.querySelector('#task4');
+  var conn = document.querySelector('#container');
 
   function delegate(event) {
       var target = event.target;
@@ -57,41 +60,49 @@ block.addEventListener('mousemove', getCoordinates);
   function changeBG(target, conn) {
       var source = target.getAttribute('src');
       conn.style.backgroundImage = 'url(' + source + ')';
-      conn.style.backgroundSize = 'contain';
-      conn.style.backgroundRepeat = 'no-repeat';
-      conn.style.backgroundPosition = 'center';
   }
   parentDiv.addEventListener('click', delegate);
+
   // Task5
+
   var blockTask5 = document.querySelector('#task5');
-  blockTask5.style.top="0";
-    function moveDown() {
-   var position =parseInt(blockTask5.style.top);
-     if (position<300) {
-  blockTask5.style.top =(position +100) +'px';
-   } else {
-    blockTask5.style.top= '0px';
-   }
+
+  function moveDown() {
+      var position = blockTask5.style.top;
+      if (position === "") {
+          blockTask5.style.top = '0';
+          position = parseInt(blockTask5.style.top);
+      } else {
+          position = parseInt(blockTask5.style.top);
+      }
+      if (position < 300) {
+          blockTask5.style.top = (position + 100) + 'px';
+      } else {
+          blockTask5.style.top = '0';
+      }
   }
-  blockTask5.addEventListener('click',moveDown);
+
+  blockTask5.addEventListener('click', moveDown);
+
   // Task 6
-  var trig =document.querySelector('#trigger');
-  trig.style.left="0";
+
+  var trig = document.querySelector('#trigger');
+  var counter = 0;
+
   function moveLeft() {
-    debugger;
-   var position =parseInt(trig.style.left);
-     if (position==0) {
-  trig.style.left =50 +'%';
-  trig.style.marginLeft="-30px";
-  console.log(1);
-   } else if (position==50) {
-    trig.style.left= '80%';
-    trig.style.marginLeft="0px";
-    console.log(2);
-   } else {
-  trig.style.left= "0px";
-  trig.style.marginLeft="0px";
-  console.log(0);
- }
-}
- trig.addEventListener('click',moveLeft);
+      var position = trig.style.marginRight
+      if (position === '') {
+          trig.style.marginRight = 'auto'
+          trig.style.marginLeft = 'auto';
+          counter++
+      } else if (position === 'auto') {
+          trig.style.marginRight = '0'
+          counter++
+      } else {
+          trig.style.marginLeft = "";
+          trig.style.marginRight = "";
+          counter = 0;
+      }
+      console.log(counter);
+  }
+  trig.addEventListener('click', moveLeft);
