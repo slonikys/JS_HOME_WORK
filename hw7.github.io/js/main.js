5ce018d41c62dd21d5dfe6b78616c6b3c4e09cbd
Dmitrij Sotnikov <terorl2@mail.ru>
2017-11-28 18:22:38 +0200

    litle fix

--- hw7.github.io/js/main.js
+++ hw7.github.io/js/main.js
@@ -6,14 +6,13 @@ const topRatedSubmit = document.querySelector('.js-tr-button');
 const input = document.querySelector('#js-input-value');
 const gallery = document.querySelector('.gallery');
 const apiKey = '&api_key=f24a0fd18f52218851075901c5a108a0&';
-const queryUrl = `https://api.themoviedb.org/3/search/movie?language=en-US&page=1&include_adult=false`;
-const popularUrl = `https://api.themoviedb.org/3/movie/popular?&language=en-US&page=1`;
-const latestUrl = `https://api.themoviedb.org/3/movie/latest?language=en-US`;
-const topRatedUrl = `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1`;
+const queryUrl = 'https://api.themoviedb.org/3/search/movie?language=en-US&page=1&include_adult=false';
+const popularUrl = 'https://api.themoviedb.org/3/movie/popular?&language=en-US&page=1';
+const latestUrl = 'https://api.themoviedb.org/3/movie/latest?language=en-US';
+const topRatedUrl = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
 
-const fetchFilms = (url, searchQuery) =>{
-if (searchQuery !=="") {
-  return fetch(url + apiKey + `&query=${searchQuery}`)
+const fetchFilms = (url, searchQuery) =>
+   fetch(url + apiKey + `&query=${searchQuery}`)
   .then(response => {
     if (response.ok) {
       return response.json();
@@ -22,9 +21,7 @@ if (searchQuery !=="") {
     throw new Error('error while fetching, ' + response.statusText);
   })
   .catch(err => console.log(err));
-} else {
-  alert ('Введите название фильма');
-}};
+
 
 const getFilmsInfo = films =>
   films.map(elem => {
@@ -75,21 +72,23 @@ const fullCycle =(someUrl, someQuery) => {
 
 form.addEventListener('submit', (evt) => {
   evt.preventDefault();
+  if (input.value !=="") {
   fullCycle(queryUrl, input.value);
+  } else {
+    alert ('Введите имя фильма !');
+    return false;
+  }
 });
 
-popularSubmit.addEventListener('click', (evt) => {
-  evt.preventDefault();
+popularSubmit.addEventListener('click', () => {
   fullCycle(popularUrl);
 });
-latestSubmit.addEventListener('click', (evt) => {
-  evt.preventDefault();
+latestSubmit.addEventListener('click', () => {
   fetchFilms(latestUrl)
     .then(data => {
       renderGalleryLatest(data, gallery);
     });
 });
-topRatedSubmit.addEventListener('click', (evt) => {
-  evt.preventDefault();
+topRatedSubmit.addEventListener('click', () => {
   fullCycle(topRatedUrl);
   });
